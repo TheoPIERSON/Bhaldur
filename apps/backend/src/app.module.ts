4cc3ea587f9c954b923d9178829548956b917e60
Theo Pierson <thheopierson@gmail.com>
2025-05-14 16:43:05 +0200

    feat: start Nest app on port 4000

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './modules/user/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env', // ou ['.env', 'apps/backend/.env']
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
